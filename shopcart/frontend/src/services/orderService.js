6446ef5069c9258792ed96b4febd29286616db53
Pranav <mohnapranav@gmail.com>
2026-03-21 08:12:35 +0530

    Update orderService

--- shopcart/frontend/src/services/orderService.js
+++ shopcart/frontend/src/services/orderService.js
@@ -3,24 +3,15 @@ import api, { apiMultipart } from './api';
 const orderService = {
   getMyOrders: () => api.get('/orders/my'),
   getOrderById: (id) => api.get(`/orders/${id}`),
-  placeOrder: (orderData) => {
-    if (orderData.paymentScreenshot) {
-      const formData = new FormData();
-      Object.keys(orderData).forEach(key => {
-        if (key === 'paymentScreenshot') {
-          formData.append(key, orderData[key]);
-        } else if (typeof orderData[key] === 'object') {
-          formData.append(key, JSON.stringify(orderData[key]));
-        } else {
-          formData.append(key, orderData[key]);
-        }
-      });
-      return apiMultipart.post('/orders', formData, {
-        headers: { 'Content-Type': 'multipart/form-data' }
-      });
-    }
-    return api.post('/orders', orderData);
+
+  placeOrder: (formData) => {
+    // Always send as multipart/form-data since backend uses @RequestParam
+    return apiMultipart.post('/orders', formData, {
+      headers: { 'Content-Type': 'multipart/form-data' }
+    });
   },
+
   cancelOrder: (id) => api.post(`/orders/${id}/cancel`),
 };
+
 export default orderService;
